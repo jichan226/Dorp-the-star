8d353e5fc03f4b1268b68677b67244fe6e8e7c0a
jichan226 <wjdgurdt68@gmail.com>
2024-08-15 21:01:15 +0800

    양식수정

--- 소스.c
+++ 소스.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-void DrowStarLine()
+void DrawStarLine()
 {
-	int StarCount;
-	scanf_s("%d", &StarCount);
-	for (int i = 0; i < StarCount; i++)
-	printf("*");
-
+	int starCount;
+	scanf_s("%d", &starCount);
+	for (int i = 0; i < starCount; i++)
+		printf("*");
 }
 
 int main(void)
 {
-	DrowStarLine();
+	DrawStarLine();
 	return 0;
-
 }
