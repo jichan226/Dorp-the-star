3a65b02a840ffcba0b2ff8217426542ae173030a
jichan226 <wjdgurdt68@gmail.com>
2024-08-19 20:34:52 +0800

    Update 소스.c

--- 소스.c
+++ 소스.c
@@ -4,14 +4,23 @@ void DrawStarLine(int starCount)
 {
 	for (int i = 0; i < starCount; i++)
 		printf("*");
-
 	printf("\n");
 }
 
 void DrawStarSquare(int starCount, int starLine)
 {
 	for (int j = 0; j < starLine; j++)
+
 		DrawStarLine(starCount);
+
+}
+
+void DrawStartOutLine(int lenght, int width)
+{
+	for (int i = 0; i < lenght; i++)
+		printf("*");
+	printf("\n");
+
 }
 
 int main(void)
